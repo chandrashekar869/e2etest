bd94687034ece8c457f6b87c18add31ee34a676d
chandrashekar869 <shekard.chandra869@gmail.com>
2018-08-06 06:59:01 +0530

    first commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,92 @@
 import { AppPage } from './app.po';
-
-describe('vvvv App', () => {
+import { browser } from 'protractor';
+describe(' App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display login screen', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    page.presentUrl().then((x)=>{
+      console.log(x);
+      expect(x).toContain('login');
+    });
   });
+
+  it('Button enable on right data entered', () => {
+    page.setEmail("admin@yopmail.com");
+    page.setPassword("admin123");
+    expect(page.getButton()).toBeFalsy();
+  });
+
+  it('navigate to dashboard on click', () => {
+    page.loginButton().click();
+    page.presentUrl().then((x)=>{
+      console.log(x);
+      expect(x).toContain('localhost:49152');
+    });
+  });
+
+  it('display user details', () => {
+    page.getIndex().click();
+    page.presentUrl().then((x)=>{
+      console.log(x);
+      expect(x).toContain('localhost:49152/detail/0');
+    });
+  });
+
+
+  it('go back', () => {
+    page.navigateTo();
+    page.getaddUserButton().click();
+    page.setFirstName("Chandra");
+    page.setLastName("Shekar");
+    page.setAddUserEmail("shekard.chandra869@gmail.com");
+    page.setPhone("7259284709");
+    page.getRegisterButton().click();
+    page.presentUrl().then((x)=>{
+      console.log("url",x);
+      expect(x).toBe('http://localhost:49152/');
+    }); 
+  });
+  it('logout', () => {
+    page.getLogoutButton().click();
+    page.presentUrl().then((x)=>{
+      console.log("url",x);
+      expect(x).toBe('http://localhost:49152/login');
+    }); 
+  });
+
+
+  /*
+  it('should display login screen', () => {
+    page.navigateTo();
+    page.setEmail("admin@yopmail.com");
+    page.setPassword("admin123");
+    expect(page.getButton()).toBeFalsy();
+    page.loginButton().click();
+    page.presentUrl().then((x)=>{
+      console.log(x);
+      expect(x).toContain('localhost:49152');
+    });
+    browser.pause(49152);
+  });
+
+
+  it('should display login screen', () => {
+    page.navigateTo();
+    page.setEmail("admin@yopmail.com");
+    page.setPassword("admin123");
+    expect(page.getButton()).toBeFalsy();
+    page.loginButton().click();
+    page.presentUrl().then((x)=>{
+      console.log(x);
+      expect(x).toContain('localhost:49152');
+    });
+    browser.pause(49152);
+  });
+*/
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,59 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ExpectedConditions } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
+  presentUrl(){
+    return browser.getCurrentUrl();
+  }
+
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getButton() {
+    return element(by.css('app-login button')).getAttribute('disabled');
+  }
+  setEmail(email) {
+    return element(by.css('[formcontrolname="email"]')).sendKeys(email);
+  }
+  setAddUserEmail(email) {
+     browser.wait(ExpectedConditions.visibilityOf(element(by.id('email'))),500);
+     element(by.id('email')).sendKeys(email);
+    }
+  setPhone(phone) {
+    browser.wait(ExpectedConditions.visibilityOf(element(by.id('phone'))),0);
+    return element(by.id('phone')).sendKeys(phone);
+  }
+  setFirstName(fname) {
+    browser.wait(ExpectedConditions.visibilityOf(element(by.id('first_name'))),0);
+    return element(by.id('first_name')).sendKeys(fname);
+  }
+  setLastName(lname) {
+    browser.wait(ExpectedConditions.visibilityOf(element(by.id('last_name'))),0);
+    return element(by.id('last_name')).sendKeys(lname);
+  }
+  setPassword(password) {
+    return element(by.css('[formcontrolname="password"]')).sendKeys(password);
+  }
+  loginButton() {
+    return element(by.css('app-login button'));
+  }
+  getIndex(){
+    return element(by.id('1'));
+  }
+  getBackButton(){
+    return element(by.css('[routerlink="/"]'));
+  }
+  getaddUserButton(){
+    return element(by.css('app-home nav [routerlink="/add"]'));
+  }
+  getRegisterButton(){
+    return element(by.css('app-student-add button'));
+  }
+  getLogoutButton(){
+    return element(by.id('logout'));
   }
 }
